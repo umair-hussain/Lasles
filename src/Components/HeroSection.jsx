abe143a2717b8dffbf5be32af671646fd046715d
umair-hussain <66690667+umair-hussain@users.noreply.github.com>
2026-01-18 02:20:58 +0500

    Update HeroSection.jsx

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -38,7 +38,7 @@ const HeroSection = () => {
             <img src="/Illustration2.png" alt="illustration" />
 
             <div className="absolute top-0 float">
-              <img src="/Lasles/lock.svg" className="w-15 md:w-full" />
+              <img src="/lock.svg" className="w-15 md:w-full" />
             </div>
 
             <div className="absolute top-20 left-5 float md:top-28 md:left-16">
